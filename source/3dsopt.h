bc50bf14b1b08fe119c8148ee2bc3fc840a95025
Wyatt-James <bomberbeast44@gmail.com>
2026-02-07 18:48:21 -0600

    Profiler Improvements - The profiler now uses stdint.h types - The profiler no longer uses inline functions for timer operations - Fixed printing of counter clocks - Recorded which profiler slots are in-use in 3dsopt.h

--- source/3dsopt.h
+++ source/3dsopt.h
@@ -1,19 +1,49 @@
-
 #ifndef _3DSOPT_H_
 #define _3DSOPT_H_
 
-#include <3ds.h>
+#include <stdint.h>
 
-#include "snes9x.h"
-#include "port.h"
-#include "memmap.h"
-#include "3dssnes9x.h"
+/*
+ * Currently used clocks:
+ * 1  - aptMainLoop (total)
+ * 2  - SuperFX
+ * 3  - CopyFB
+ * 4  - Flush
+ * 5  - Transfer
+ * 
+ * 11 - S9xUpdateScreen
+ * 
+ * 20 - Read 214x (disabled)
+ * 21 - DrawBG0, APU_EXECUTE (disabled), Write 214x (disabled)
+ * 22 - DrawBG1
+ * 23 - DrawBG2
+ * 24 - DrawBG3
+ * 25 - DrawBKClr
+ * 26 - DrawOBJS
+ * 27 - DrawBG0_M7
+ * 28 - S9xSetupOBJ
+ * 29 - Colormath
+ * 30 - DrawWindowStencils, ComputeClipWindows (disabled)
+ * 31 - RenderScnHW
+ * 
+ *  Audio Mixer (CPU1)
+ * 41 - Mix-Timing
+ * 42 - Mix-ApplyMstVol
+ * 43 - Mix-Flush
+ * 44 - Mix-Replay+S9xMix
+ * 
+ * 51 - APU
+ * 70 - PrepM7
+ * 71 - PrepM7-Palette
+ * 72 - PrepM7-FullTile
+ * 73 - PrepM7-CharFlag
+*/
 
 typedef struct
 {
     char *name;
     int count;
-    u64 startTick, totalTicks;
+    uint64_t startTick, totalTicks;
 } T3DS_Clock;
 
 #ifndef RELEASE
@@ -22,26 +52,15 @@ typedef struct
 extern T3DS_Clock t3dsClocks[T3DS_NUM_CLOCKS];
 
 void t3dsResetTimings(void);
-void t3dsCount(int bucket, char *name);
 void t3dsShowTotalTiming(int bucket);
-void t3dsSetTotalForPercentage(u64 time);
-void t3dsSetCountForPercentage(u64 count);
+void t3dsSetTotalForPercentage(uint64_t time);
+void t3dsSetCountForPercentage(uint64_t count);
+void t3dsCount(int bucket, char *clockName);
+void t3dsStartTiming(int bucket, char *clockName);
+void t3dsEndTiming(int bucket);
 
-static inline void t3dsStartTiming(int bucket, char *clockName)
-{
-    t3dsClocks[bucket].startTick = svcGetSystemTick();
-    t3dsClocks[bucket].name = clockName;
-}
-
-static inline void t3dsEndTiming(int bucket)
-{
-    T3DS_Clock* clock = &t3dsClocks[bucket];
-    clock->totalTicks += (svcGetSystemTick() - clock->startTick);
-    clock->count++;
-}
-
-static inline u64 t3dsGetTime(int bucket) {return t3dsClocks[bucket].totalTicks;}
-static inline u64 t3dsGetCount(int bucket) {return t3dsClocks[bucket].count;}
+static inline uint64_t t3dsGetTime(int bucket) {return t3dsClocks[bucket].totalTicks;}
+static inline uint64_t t3dsGetCount(int bucket) {return t3dsClocks[bucket].count;}
 
 #else // RELEASE
 #define T3DS_NUM_CLOCKS 0
@@ -50,9 +69,9 @@ static inline void t3dsCount(int bucket, char *name)            {} // Stub
 static inline void t3dsShowTotalTiming(int bucket)              {} // Stub
 static inline void t3dsStartTiming(int bucket, char *clockName) {} // Stub
 static inline void t3dsEndTiming(int bucket)                    {} // Stub
-static inline void t3dsSetTotalForPercentage(u64 time)          {} // Stub
-static inline u64  t3dsGetTime(int bucket)                      {return 0;} // Stub
-static inline u64  t3dsGetCount(int bucket)                     {return 0;} // Stub
+static inline void t3dsSetTotalForPercentage(uint64_t time)     {} // Stub
+static inline uint64_t t3dsGetTime(int bucket)                  {return 0;} // Stub
+static inline uint64_t t3dsGetCount(int bucket)                 {return 0;} // Stub
 #endif // RELEASE
 
 #endif
