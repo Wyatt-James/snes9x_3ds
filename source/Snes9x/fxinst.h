392c1f585af49e848311aa7e54f084ef11054913
Wyatt-James <bomberbeast44@gmail.com>
2026-06-09 00:44:18 -0500

    Amend: Correct PLOT_DITHER Documentation

--- source/Snes9x/fxinst.h
+++ source/Snes9x/fxinst.h
@@ -359,7 +359,7 @@ struct FxRegs_s
 #define CLSR USEX8(GSU.pvRegisters[GSU_CLSR])
 
 #define PLOT_TRANSPARENT (1U << 0) // 0x01. If clear, transparent colors will not be drawn.
-#define PLOT_DITHER      (1U << 1) // 0x02. If set, even pixels will use the top nibble of COLOR and odd pixels the bottom nibble
+#define PLOT_DITHER      (1U << 1) // 0x02. If set, draws pixels with a dither pattern, alternating between the top and bottom nibbles of COLOR
 #define PLOT_HIGHNIBBLE  (1U << 2) // 0x04. If set, COLR and GETC will replace the color's low nibble with its high nibble
 #define PLOT_FREEZEHIGH  (1U << 3) // 0x08. If set, COLR and GETC will only modify the low nibble
 #define PLOT_OBJECT      (1U << 4) // 0x10. 
